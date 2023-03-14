91f8ff3c86e969335c8d6e69409839df6e413b39
Kraisorn Kuiraksa <s6335510011@sau.ac.th>
2023-03-14 21:04:57 +0700

    Second_Com

--- model/Product.php
+++ model/Product.php
@@ -94,9 +94,3 @@ class Product
     }
 }
 
-// Q: how to push this project to github aga
-// A: git init
-//    git add .
-//    git commit -m "first commit"
-//    git remote add origin
-//    git push -u origin master
